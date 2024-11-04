59045e9334d1a5d7b227305c18d78f259ec110cc
bilal-patel <112193065+bilal-patel@users.noreply.github.com>
2024-11-04 02:11:39 +0000

    Implement async/await to fetch ticket data and handle errors.

---
+++ script.js
@@ -0,0 +1,38 @@
+// Function to make the HTTP request to the API using fetch 
+async function fetchAndDisplayUnresolvedTickets() {
+  // TASK 2
+  // Implement try catch
+  try {
+    response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // Get the relevant data from the response
+    const data = await response.json();
+    // Get the sections that we will add dynamic data into
+    const error = document.getElementById('ticket-fetch-error');
+    const ticketSection = document.getElementById('tickets');
+
+    // Error throwing
+    if (data.length === 0) {
+      throw new Error("No tickets found!");
+    }
+    if (!response.ok) {
+      throw new Error("Failed to fetch tickets!");
+    }
+    data.forEach(ticket => {
+      const ticketDiv = document.createElement('div');
+      ticketDiv.innerHTML = `
+        <h2>Ticket Id: ${ticket.id}</h2>
+        <p>Customer Name: ${ticket.userId}</p>
+        <p>Issue Description: ${ticket.title}</p>
+        <p>Details: ${ticket.body}</p>
+      `;
+      ticketSection.appendChild(ticketDiv);
+    })
+    // Handling error and setting the content of the error section
+  } catch (e) {
+    error.textContent = `Error: Fetch failed ${e.message}`;
+  } finally {
+    console.log("Fetch completed");
+  }
+}
+// call main function
+fetchAndDisplayUnresolvedTickets();
